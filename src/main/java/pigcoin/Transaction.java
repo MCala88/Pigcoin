91e2875fd99707e6af7d58503bf6f2f6a622fb2e
Cala 88 <mcalafell@cifpfbmoll.eu>
2020-03-02 16:22:47 +0100

    Start

---
+++ src/main/java/pigcoin/Transaction.java
@@ -0,0 +1,12 @@
+package pigcoin;
+
+public class Transaction {
+	private String hash = null;
+	private String prev_hash = null;
+	private String pigcoin = null;
+	private String Message = "Transaccion realizada";
+	private String byte[];
+	
+	
+	
+}

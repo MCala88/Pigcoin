a33e97f5e61e38d556c8a715d5e21418d9f41dd1
Cala 88 <mcalafell@cifpfbmoll.eu>
2020-03-10 13:16:57 +0100

    Ultimos metodos

--- src/main/java/pigcoin/BlockChain.java
+++ src/main/java/pigcoin/BlockChain.java
@@ -3,6 +3,7 @@ package pigcoin;
 import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -56,5 +57,46 @@ public class BlockChain {
     	double[] pigcoins = {pigcoinsIn, pigcoinsOut};
     	return pigcoins;
     }
-		
+
+    public boolean isSignatureValid(PublicKey pKey_sender, String message, byte[] signedTransaction) {
+    	return GenSig.verify(pKey_sender, message, signedTransaction);
+    }
+    
+    public boolean isConsumedCoinValid(Map<String, Double> consumedCoins) {
+    	for (String hash :consumedCoins.keySet()) {
+    		for (Transaction transaction : blockChain) {
+    			if (hash.equals(transaction.getPrev_hash())) {
+    				return false;
+    			}
+    		}
+    	}
+    	return true;
+    }
+    
+    /* Codigo no original */
+    public void createTransaction(PublicKey pKey_sender, PublicKey pKey_recipient, Map<String, Double> consumedCoins, String message, byte[] signedTransaction) {
+    
+    	PublicKey address_recipient = pKey_recipient;
+        Integer lastBlock = 0;
+
+        for (String prev_hash: consumedCoins.keySet()) {
+            if (prev_hash.startsWith("CA_")) {
+                pKey_recipient = pKey_sender;
+            }
+            
+            lastBlock = blockChain.size() + 1;
+            Transaction transaction = new Transaction("hash_" + lastBlock.toString(), prev_hash, pKey_sender,
+                    pKey_recipient, consumedCoins.get(prev_hash), message);
+            getBlockChain().add(transaction);
+            
+            pKey_recipient = address_recipient;
+        }
+    }
+    
+    public void processTransactions(PublicKey pKey_sender, PublicKey pKey_recipient, Map<String, Double> consumedCoins, String message, byte[] signedTransaction) {
+    
+    	if (isSignatureValid(pKey_recipient, message, signedTransaction) && isConsumedCoinValid(consumedCoins)) {
+    		createTransaction(pKey_recipient, pKey_recipient, consumedCoins, message, signedTransaction);
+    	}
+    }
 }
--- src/main/java/pigcoin/Wallet.java
+++ src/main/java/pigcoin/Wallet.java
@@ -4,9 +4,11 @@ import java.security.KeyPair;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Wallet {
 	private PublicKey address = null;
@@ -69,20 +71,17 @@ public class Wallet {
 	}
 	
 	public void loadInputTransactions(BlockChain bChain) {
-		bChain.getBlockChain().stream().filter(transaction -> transaction.getpKey_recipient().equals(getAddress())).forEachOrdered(transaction->{inputTransactions.add(transaction);});	
+		setInputTransactions(bChain.loadInputTransaction(getAddress()));
 	}
 	
 	public void loadOutputTransactions(BlockChain bChain) {
-		bChain.getBlockChain().stream().filter(transaction -> transaction.getpKey_sender().equals(getAddress())).forEachOrdered(transaction->{outputTransactions.add(transaction);});	
+		setOutputTransactions(bChain.loadOutputTransaction(getAddress()));	
 	}
 	
 	public byte[] signTransaction(String message)  {
 		return GenSig.sign(getsKey(), message);
 	}
-	
-	public String toString() {
-		return " wallet: " + "\n" + "Total input: " + total_input + "Total output: " + total_output + "\n" + "Balances: " + balance + "\n";
-	}
+
 	
     public void updateBalance() {
 		this.balance = this.getTotalInput() - this.getTotalOutput();
@@ -98,13 +97,65 @@ public class Wallet {
 	}
 		
 		
-	public void sendCoins(PublicKey address2, double d, String string, BlockChain bChain) {
-		// TODO Auto-generated method stub
+	public void sendCoins(PublicKey pKey_recipient, double coins, String message, BlockChain bChain) {
 		
+		Map<String, Double> consumedCoins = new LinkedHashMap<>();
+		
+		consumedCoins = collectCoins(coins);
+		
+		if (consumedCoins != null) {
+			bChain.processTransactions(getAddress(), pKey_recipient, consumedCoins, message, signTransaction(message));
+		}
 	}
+	
+	/* Metodo mayoritariamente no original */
 	public Map<String, Double> collectCoins(Double pigcoins) {
-		Map<String, Double> collectCoins = new LinkedHashMap<>();
+		Map<String, Double> collectedCoins = new LinkedHashMap<>();
+		
+		if (getInputTransactions() == null||pigcoins > getBalance()) {
+			
 		return null;
+		}
+		
+		Double coinsGuardadas = 0d;
+		
+		Set<String> consumedCoins = new HashSet<>();
+		  
+		 if (getOutputTransactions() != null) {
+			for (Transaction transaction : getOutputTransactions()) {
+				consumedCoins.add(transaction.getPrev_hash());
+			}
+		} 
+	        for (Transaction transaction : getInputTransactions()) {
+
+	            if (consumedCoins.contains(transaction.getHash())) {
+	                continue;
+	            }
+
+	            if (transaction.getPigcoin() == pigcoins) {
+	                collectedCoins.put(transaction.getHash(), transaction.getPigcoin());
+	                consumedCoins.add(transaction.getHash());
+	                break;
+	            } else if (transaction.getPigcoin() > pigcoins) {
+	                collectedCoins.put(transaction.getHash(), pigcoins);
+	                collectedCoins.put("CA_" + transaction.getHash(), transaction.getPigcoin() - pigcoins);
+	                consumedCoins.add(transaction.getHash());
+	                break;
+	            } else {
+	                collectedCoins.put(transaction.getHash(), transaction.getPigcoin());
+	                coinsGuardadas = transaction.getPigcoin();
+	                pigcoins = pigcoins - coinsGuardadas;
+	                consumedCoins.add(transaction.getHash());
+	            }
+
+	        }
+	        // getInputTransactions().removeAll(consumedCoins);
+	        return collectedCoins;
+	}
+	
+	@Override
+	public String toString() {
+		return "\n" + "wallet: " + getAddress().hashCode() + "\n" + "Total input: " + getTotalInput() + "\n" + "Total output: " + getTotalOutput() + "\n" + "Balances: " + getBalance() + "\n";
 	}
 		
 }

f9e966ac92b12584990df2b90dcf23854036fc4d
Cala 88 <mcalafell@cifpfbmoll.eu>
2020-03-11 11:59:05 +0100

    Encapsulación

--- src/main/java/pigcoin/Transaction.java
+++ src/main/java/pigcoin/Transaction.java
@@ -22,61 +22,34 @@ public class Transaction {
         this.message = message;
     }
 	
-	public String getHash() {
+	String getHash() {
 		return hash;
 	}
 
-	public void setHash(String hash) {
-		this.hash = hash;
-	}
-
-	public String getPrev_hash() {
+	String getPrev_hash() {
 		return prev_hash;
 	}
 
-	public void setPrev_hash(String prev_hash) {
-		this.prev_hash = prev_hash;
-	}
-
-	public double getPigcoin() {
+	double getPigcoin() {
 		return pigcoins;
 	}
 
-	public void setPigcoin(double pigcoin) {
-		this.pigcoins = pigcoin;
-	}
-
-	public String getMessage() {
+	String getMessage() {
 		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
 	}
 
 	public byte[] getSignature() {
 		return Signature;
 	}
-
-	public void setSignature(byte[] signature) {
-		Signature = signature;
-	} 
 	
 	public PublicKey getpKey_sender() {
 		return pKey_sender;
-	}
-
-	public void setpKey_sender(PublicKey pKey_sender) {
-		this.pKey_sender = pKey_sender;
 	}
 
 	public PublicKey getpKey_recipient() {
 		return pKey_recipient;
 	}
 
-	public void setpKey_recipient(PublicKey pKey_recipient) {
-		this.pKey_recipient = pKey_recipient;
-	}
 
 	@Override
 	public String toString() {
